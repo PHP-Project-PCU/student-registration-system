f09f69ea084df4a1123a6b9c39d3fd55f181b9b3
Sue Yan Shin <sueyanshin@gmail.com>
2024-08-06 18:39:19 +0630

    registration UI

--- src/core/helpers/Constants.php
+++ src/core/helpers/Constants.php
@@ -4,5 +4,7 @@ namespace core\helpers;
 
 class Constants
 {
-    static $BASE_URL = 'C:\xampp\htdocs\student-registration-system';
+    static $BASE_PATH = 'C:\xampp\htdocs\student-registration-system';
+    static $ADMIN_BASE_URL = 'http://admin.ucspyay.edu';
+    static $BASE_URL = 'http://ucspyay.edu';
 }
