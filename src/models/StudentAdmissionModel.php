c572329a6cd1753e4c372b7b2cbed6975a755c12
Sue Yan Shin <sueyanshin@gmail.com>
2024-08-20 08:24:57 +0630

    admission model fix

--- src/models/StudentAdmissionModel.php
+++ src/models/StudentAdmissionModel.php
@@ -38,31 +38,31 @@ class StudentAdmissionModel
             )";
             $stmt_student = $this->db->prepare($sql_student);
 
-            $student_nrc = $_POST['student_nrc_code'] . $_POST['student_nrc_name'] . $_POST['student_nrc_type'] . $_POST['student_nrc_num'];
+            $student_nrc = $data['student_nrc_code'] . $data['student_nrc_name'] . $data['student_nrc_type'] . $data['student_nrc_num'];
 
             $stmt_student->execute([
-                ':year' => $_POST['year'],
-                ':major' => $_POST['major'],
-                ':student_name_my' => $_POST['student_name_my'],
-                ':student_name_en' => $_POST['student_name_en'],
-                ':student_ethnicity' => $_POST['student_ethnicity'],
-                ':student_religion' => $_POST['student_religion'],
-                ':student_birth_place' => $_POST['student_birth_place'],
-                ':student_nationality' => $_POST['student_nationality'],
-                ':student_dob' => $_POST['student_dob'],
-                ':student_email' => $_POST['student_email'],
-                ':student_phone_num' => $_POST['student_phone_num'],
-                ':roll_num' => $_POST['roll_num'] ?? "",
-                ':matriculation_reg_num' => $_POST['matriculation_reg_num'],
-                ':started_year' => $_POST['started_year'],
-                ':matriculation_roll_num' => $_POST['matriculation_roll_num'],
-                ':matriculation_year' => $_POST['matriculation_year'],
-                ':matriculation_exam_center' => $_POST['matriculation_exam_center'],
+                ':year' => $data['year'],
+                ':major' => $data['major'],
+                ':student_name_my' => $data['student_name_my'],
+                ':student_name_en' => $data['student_name_en'],
+                ':student_ethnicity' => $data['student_ethnicity'],
+                ':student_religion' => $data['student_religion'],
+                ':student_birth_place' => $data['student_birth_place'],
+                ':student_nationality' => $data['student_nationality'],
+                ':student_dob' => $data['student_dob'],
+                ':student_email' => $data['student_email'],
+                ':student_phone_num' => $data['student_phone_num'],
+                ':roll_num' => $data['roll_num'] ?? "",
+                ':matriculation_reg_num' => $data['matriculation_reg_num'],
+                ':started_year' => $data['started_year'],
+                ':matriculation_roll_num' => $data['matriculation_roll_num'],
+                ':matriculation_year' => $data['matriculation_year'],
+                ':matriculation_exam_center' => $data['matriculation_exam_center'],
                 ':student_nrc' => $student_nrc ?? "",
-                ':student_region' => $_POST['student_region'],
-                ':student_township' => $_POST['student_township'],
-                ':student_current_address' => $_POST['student_current_address'],
-                ':scholarship' => $_POST['scholarship'],
+                ':student_region' => $data['student_region'],
+                ':student_township' => $data['student_township'],
+                ':student_current_address' => $data['student_current_address'],
+                ':scholarship' => $data['scholarship'],
                 ':status' => 0,
             ]);
 
@@ -90,36 +90,36 @@ class StudentAdmissionModel
                 :student_moth_address, :student_moth_job, :student_moth_phone_num
             )";
 
-            $student_fath_nrc = $_POST['student_fath_nrc_code'] . $_POST['student_fath_nrc_name'] . $_POST['student_fath_nrc_type'] . $_POST['student_fath_nrc_num'];
-            $student_moth_nrc = $_POST['student_moth_nrc_code'] . $_POST['student_moth_nrc_name'] . $_POST['student_moth_nrc_type'] . $_POST['student_moth_nrc_num'];
+            $student_fath_nrc = $data['student_fath_nrc_code'] . $data['student_fath_nrc_name'] . $data['student_fath_nrc_type'] . $data['student_fath_nrc_num'];
+            $student_moth_nrc = $data['student_moth_nrc_code'] . $data['student_moth_nrc_name'] . $data['student_moth_nrc_type'] . $data['student_moth_nrc_num'];
 
             $stmt_parent = $this->db->prepare($sql_parent);
             $stmt_parent->execute([
                 ':student_id' => $student_id,
-                ':student_fath_name_my' => $_POST['student_fath_name_my'],
-                ':student_fath_name_en' => $_POST['student_fath_name_en'],
-                ':student_fath_ethnicity' => $_POST['student_fath_ethnicity'],
-                ':student_fath_religion' => $_POST['student_fath_religion'],
-                ':student_fath_birth_place' => $_POST['student_fath_birth_place'],
-                ':student_fath_nationality' => $_POST['student_fath_nationality'],
+                ':student_fath_name_my' => $data['student_fath_name_my'],
+                ':student_fath_name_en' => $data['student_fath_name_en'],
+                ':student_fath_ethnicity' => $data['student_fath_ethnicity'],
+                ':student_fath_religion' => $data['student_fath_religion'],
+                ':student_fath_birth_place' => $data['student_fath_birth_place'],
+                ':student_fath_nationality' => $data['student_fath_nationality'],
                 ':student_fath_nrc' => $student_fath_nrc,
-                ':student_fath_region' => $_POST['student_fath_region'],
-                ':student_fath_township' => $_POST['student_fath_township'],
-                ':student_fath_address' => $_POST['student_fath_address'],
-                ':student_fath_job' => $_POST['student_fath_job'],
-                ':student_fath_phone_num' => $_POST['student_fath_phone_num'],
-                ':student_moth_name_my' => $_POST['student_moth_name_my'],
-                ':student_moth_name_en' => $_POST['student_moth_name_en'],
-                ':student_moth_ethnicity' => $_POST['student_moth_ethnicity'],
-                ':student_moth_religion' => $_POST['student_moth_religion'],
-                ':student_moth_birth_place' => $_POST['student_moth_birth_place'],
-                ':student_moth_nationality' => $_POST['student_moth_nationality'],
+                ':student_fath_region' => $data['student_fath_region'],
+                ':student_fath_township' => $data['student_fath_township'],
+                ':student_fath_address' => $data['student_fath_address'],
+                ':student_fath_job' => $data['student_fath_job'],
+                ':student_fath_phone_num' => $data['student_fath_phone_num'],
+                ':student_moth_name_my' => $data['student_moth_name_my'],
+                ':student_moth_name_en' => $data['student_moth_name_en'],
+                ':student_moth_ethnicity' => $data['student_moth_ethnicity'],
+                ':student_moth_religion' => $data['student_moth_religion'],
+                ':student_moth_birth_place' => $data['student_moth_birth_place'],
+                ':student_moth_nationality' => $data['student_moth_nationality'],
                 ':student_moth_nrc' => $student_moth_nrc,
-                ':student_moth_region' => $_POST['student_moth_region'],
-                ':student_moth_township' => $_POST['student_moth_township'],
-                ':student_moth_address' => $_POST['student_moth_address'],
-                ':student_moth_job' => $_POST['student_moth_job'],
-                ':student_moth_phone_num' => $_POST['student_moth_phone_num']
+                ':student_moth_region' => $data['student_moth_region'],
+                ':student_moth_township' => $data['student_moth_township'],
+                ':student_moth_address' => $data['student_moth_address'],
+                ':student_moth_job' => $data['student_moth_job'],
+                ':student_moth_phone_num' => $data['student_moth_phone_num']
             ]);
 
             $sql_guardian = "INSERT INTO student_guardian_tbl (
@@ -140,15 +140,15 @@ class StudentAdmissionModel
             $stmt_guardian = $this->db->prepare($sql_guardian);
             $stmt_guardian->execute([
                 ':student_id' => $student_id,
-                ':guardian_name' => $_POST['guardian_name'],
-                ':guardian_relation' => $_POST['guardian_relation'],
-                ':guardian_job' => $_POST['guardian_job'],
-                ':guardian_address' => $_POST['guardian_address'],
-                ':guardian_phone_num' => $_POST['guardian_phone_num'],
+                ':guardian_name' => $data['guardian_name'],
+                ':guardian_relation' => $data['guardian_relation'],
+                ':guardian_job' => $data['guardian_job'],
+                ':guardian_address' => $data['guardian_address'],
+                ':guardian_phone_num' => $data['guardian_phone_num'],
             ]);
 
             // Create a directory for the student if it doesn't exist
-            $uploadDir = '../uploads/' . $student_id . '/';
+            $uploadDir = 'C:\xampp\htdocs\student-registration-system\www\utils\uploads\admission/' . $student_id . '/';
             if (!file_exists($uploadDir)) {
                 mkdir($uploadDir, 0777, true);
             }
